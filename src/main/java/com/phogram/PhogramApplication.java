a5e4a78296d25d3e16bc91e6983648c3945b7702
gavin <dreamworld612@gmail.com>
2017-05-05 16:35:05 +0900

    first commit

---
+++ src/main/java/com/phogram/PhogramApplication.java
@@ -0,0 +1,12 @@
+package com.phogram;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PhogramApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PhogramApplication.class, args);
+	}
+}
